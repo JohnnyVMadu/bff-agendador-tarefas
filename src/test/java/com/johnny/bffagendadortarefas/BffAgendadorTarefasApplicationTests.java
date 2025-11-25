7c8eacbca6d1c4139ca832b69715e351dbc35ac6
Johnny <joanissonvieira@hotmail.com>
2025-11-25 19:33:40 -0300

    adicionado esqueleto do projeto

---
+++ src/test/java/com/johnny/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
